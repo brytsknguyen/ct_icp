e33f8cb693e4f68f462114f4af3a16a920f4e628
brytsknguyen <thienminh.npn@gmail.com>
2023-07-15 16:44:38 +0800

    fixed the script

--- install.sh
+++ install.sh
@@ -1,19 +1,29 @@
 #!bin/bash
 
-WS_DIR=/home/tmn/dev_ws/src
+WS_DIR=/home/tmn/ros_ws/dev_ws/src
 CTICP_ROOT=$(pwd)
 
 echo root dir: $CTICP_ROOT
 
-mkdir .cmake-build-superbuild && cd .cmake-build-superbuild     #< Creates the cmake folder
-cmake ../superbuild                                             #< (1) Configure step 
-cmake --build . --config Release                                #< Build step (Downloads and install the dependencies), add -DWITH_VIZ3D=ON to install with the GUI
+# Install superbuild
+mkdir .cmake-build-superbuild && cd .cmake-build-superbuild           #< Creates the cmake folder
+cmake ../superbuild                                                   #< (1) Configure step 
+cmake --build . --config Release --parallel 32                        #< Build step (Downloads and install the dependencies), add -DWITH_VIZ3D=ON to install with the GUI
 
 # Inside the main directory
 cd $CTICP_ROOT
 mkdir cmake-build-release && cd cmake-build-release                   #< Create the build directory
 cmake .. -DCMAKE_BUILD_TYPE=Release                                   #< (2) Configure with the desired options (specify arguments with -D<arg_name>=<arg_value>), add -DWITH_VIZ3D=ON to install with the GUI
-cmake --build . --target install --config Release --parallel 12       #< Build and Install the project
+cmake --build . --target install --config Release --parallel 32       #< Build and Install the project
+
+# Install roscore
+cd $CTICP_ROOT
+cd ros/roscore
+mkdir cmake-build-release && cd cmake-build-release                   #< Create the build directory
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+      -DPYTHON_INCLUDE_DIR=/usr/include/python3.8 ..                  #< (2) Configure with the desired options (specify arguments with -D<arg_name>=<arg_value>)
+cmake --build . --target install --config Release --parallel 32       #< Build and Install the ROSCore library
 
 # Make a symbolic link
 cd $CTICP_ROOT
